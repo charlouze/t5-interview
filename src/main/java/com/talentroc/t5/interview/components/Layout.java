160143362a751863c5ad11f11100a20569475534
Charlouze <me@charlouze.com>
2015-03-07 14:08:38 +0100

    Bootstrapifying

--- src/main/java/com/talentroc/t5/interview/components/Layout.java
+++ src/main/java/com/talentroc/t5/interview/components/Layout.java
@@ -1,17 +1,15 @@
 package com.talentroc.t5.interview.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
-import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 /**
  * Layout component for pages of application t5-interview.
  */
-@Import(stylesheet = "context:layout/layout.css")
-public class Layout
-{
+public class Layout {
     /**
      * The page title, for the <title> element and the <h1> element.
      */
@@ -22,32 +20,16 @@ public class Layout
     @Property
     private String pageName;
 
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private String sidebarTitle;
-
-    @Property
-    @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private Block sidebar;
-
     @Inject
     private ComponentResources resources;
 
-    @Property
-    @Inject
-    @Symbol(SymbolConstants.APPLICATION_VERSION)
-    private String appVersion;
-
-
-    public String getClassForPageName()
-    {
+    public String getClassForPageName() {
         return resources.getPageName().equalsIgnoreCase(pageName)
-                ? "current_page_item"
+                ? "active"
                 : null;
     }
 
-    public String[] getPageNames()
-    {
+    public String[] getPageNames() {
         return new String[]{"Index", "About", "Contact"};
     }
 }
